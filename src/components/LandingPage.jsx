c2e8165e8df08f7e63aacae9631fa2811c4d6b66
jjartm28 <montuya.jelenajane0528@gmail.com>
2026-04-09 21:57:52 +0800

    Push complete Festival of Fire SK Summer League 2026 React application

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+    return (
+        <div className='landing-page'>
+            <h1 className='glow'>Festival of Fire SK Summer League 2026</h1>
+            {/* Add glowing animations and graphics here */}
+            <p>Join us for an exciting summer of sports!</p>
+        </div>
+    );
+};
+
+export default LandingPage;
